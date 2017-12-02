75159e8eed1279950d31128655574f10a9bd2e92
Lance Valera <lancevalera@Lances-MacBook-Pro.local>
2017-12-02 18:27:13 -0500

    Navbar and main body components added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Profile from './Profile/profile.js'
+import Navbar from './Navbar/navbar.js';
+import Main from './MainContainer/mainbody.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <Navbar />
+        <Main />
       </div>
     );
   }
