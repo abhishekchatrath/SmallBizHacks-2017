c8c816c27ad8d29b0dd249d94effe66b6696dfd2
Gwen Li <guanyan1995@gmail.com>
2017-12-02 19:26:48 -0500

    add router to app.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import Profile from './Profile/profile.js'
 import Navbar from './Navbar/navbar.js';
 import Main from './MainContainer/mainbody.js';
+import {
+  Router,
+} from 'react-router-dom';
+import history from './history';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-        <Navbar />
-        <Main />
-      </div>
+      <Router history={history}>
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+          <Navbar />
+          <Main />
+        </div>
+      </Router>
     );
   }
 }
