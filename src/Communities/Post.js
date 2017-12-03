63332f5ba42da41518a049e9636763d8655a079a
Gwen Li <guanyan1995@gmail.com>
2017-12-02 23:23:57 -0500

    modify post page

--- src/Communities/Post.js
+++ src/Communities/Post.js
@@ -5,19 +5,22 @@ class Post extends React.Component {
   render() {
     return (
       <div className="Post">
-            <header class="mdl-layout__header">
+            <header className="mdl-layout__header postheader">
+                Post Title
             </header>
                         
-        <div classNanme="mdl-grid">
-            <div class="mdl-cell mdl-cell--4-col">
-                <img src='https://static.pexels.com/photos/220453/pexels-photo-220453.jpeg' alt='' width="100" />
-
+        <div className="mdl-grid major Profilepic">
+            <div className="mdl-cell mdl-cell--4-col">
+                <img className="profilepic" src='https://static.pexels.com/photos/220453/pexels-photo-220453.jpeg' alt='' width="100" />
             </div>
-            <div class="mdl-cell mdl-cell--8-col">
+            <div className="mdl-cell mdl-cell--8-col content">
                 Content
             </div>
+            
         </div>
-      </div>
+        
+    </div>
+   
     )
   }
 }
