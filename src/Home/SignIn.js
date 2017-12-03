a15f6934948db441666b4a0a96d1e10a62b79be8
Gwen Li <guanyan1995@gmail.com>
2017-12-03 09:19:41 -0500

    fix login page

--- src/Home/SignIn.js
+++ src/Home/SignIn.js
@@ -15,12 +15,12 @@ class SignIn extends React.Component {
             <div className="SignIn">
             <form action="#">
               <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                <input className="mdl-textfield__input" type="text" id="sample3" placeholder="Username" />
+                <input className="mdl-textfield__input ipf" type="text" id="sample3" placeholder="Username" />
                 <br />
-                <input className="mdl-textfield__input" type="text" id="sample3" placeholder="Password" />
+                <input className="mdl-textfield__input ipf" type="text" id="sample3" placeholder="Password" />
 
               </div>
-              <div>
+              <div className="thebutts">
               <button className="mdl-button mdl-js-button mdl-js-ripple-effect REGISTER">
                 REGISTER
               </button>
