faddc21b566bf74f88f1af94d3d0b5b3cbf34b86
Lance Valera <lancevalera@Lances-MacBook-Pro.local>
2017-12-03 03:26:30 -0500

    Finished Styling

--- src/Communities/Post.js
+++ src/Communities/Post.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import './Post.css';
 
+import dudeone from './B&WDude.jpg';
+import dudetwo from './MaddieAsADude.jpg';
+import dudethree from './OnPointBeard.jpg';
+
 class Post extends React.Component {
     constructor(props) {
         super(props);
     }
   render() {
     return (
+
+
+
       <div className="Post">
             <header className="mdl-layout__header postheader">
                 SUPPLIER
             </header>
-                        
+
         <div className="mdl-grid major Profilepic">
             <div className="mdl-cell mdl-cell--4-col userdetail">
                 <div className="useinfo">
@@ -27,7 +34,7 @@ class Post extends React.Component {
             <div className="mdl-cell mdl-cell--8-col content">
                 <div className="contentDiv">
                     <div className="contenttittle">
-                        Content
+                        MY SUPPLIER COND ME
                     </div>
                     <div className="contentbody">
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vul
@@ -36,10 +43,78 @@ class Post extends React.Component {
     Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl
                     </div>
 
+
+
             </div>
-            
+            <h5> COMMENTS </h5>
+            <div className='CommentsContainer'>
+
+            <div className='ProfilePicContainer'>
+            <input type="image" src={dudeone} className="CircleButton"/>
+            </div>
+
+              <div className='CommentsContent'>
+              <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vul
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+              </p>
+              </div>
+              <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect ActivatedButton">
+                <i className="material-icons">star_rate</i>
+                </button>
+            </div>
+
+            <div className='CommentsContainer'>
+
+            <div className='ProfilePicContainer'>
+            <input type="image" src={dudetwo} className="CircleButton"/>
+            </div>
+
+              <div className='CommentsContent'>
+              <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vul
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+              </p>
+              </div>
+              <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect ActivatedButton">
+                <i className="material-icons">star_rate</i>
+                </button>
+            </div>
+
+            <div className='CommentsContainer'>
+
+            <div className='ProfilePicContainer'>
+            <input type="image" src={dudethree} className="CircleButton"/>
+            </div>
+
+              <div className='CommentsContent'>
+              <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vul
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+              </p>
+              </div>
+              <button className="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect ActivatedButton">
+                <i className="material-icons">star_rate</i>
+                </button>
+            </div>
+
+            <div className="InputContainer">
+
+            <form className='FormContainer'>
+            <div className="mdl-textfield mdl-js-textfield">
+            <textarea className="mdl-textfield__input" type="text" rows= "2" cols='67' id="sample5" placeholder="Comments"></textarea>
+            </div>
+            <div className='Button'>
+            <button className='ActualButton'>
+              <i className="material-icons">add</i>
+            </button>
+            </div>
+            </form>
+
+            </div>
+
         </div>
-        
+
     </div>
    </div>
     )
