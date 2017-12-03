7cc54b189727981852fce6d09058548ac141b098
Gwen Li <guanyan1995@gmail.com>
2017-12-03 00:42:10 -0500

    fix header

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -4,15 +4,15 @@ import './navbar.css'
 class Navbar extends Component{
   render() {
     return(
-      <header className="mdl-layout__header">
-        <div className="mdl-layout__header-row">
+      <header className="mdl-layout__header ctnr">
+        <div className="mdl-layout__header-row ctnr">
           <span className="mdl-layout-title">Title</span>
           <div className="mdl-layout-spacer"></div>
           <nav className="mdl-navigation mdl-layout--large-screen-only">
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
-            <a className="mdl-navigation__link" href="">Link</a>
+            <a className="mdl-navigation__link  ctnr1" href="http://localhost:3000/profile" >RPOFILE</a>
+            <a className="mdl-navigation__link  ctnr1" href="http://localhost:3000/communities">COMMUNITIES</a>
+            <a className="mdl-navigation__link  ctnr1" href="http://localhost:3000/communities/post">AWARDS</a>
+            <a className="mdl-navigation__link ctnr1" href="">searchicon</a>
           </nav>
         </div>
       </header>
