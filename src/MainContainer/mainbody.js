39d0d4da5dbd81bd6190f76d1800af3ffc114f12
Gwen Li <guanyan1995@gmail.com>
2017-12-02 19:36:12 -0500

    add routers to mainbody.js

--- src/MainContainer/mainbody.js
+++ src/MainContainer/mainbody.js
@@ -1,12 +1,16 @@
 import React  from 'react';
 import './mainbody.css'
 import Profile from '../Profile/profile.js'
-
+import {Route} from 'react-router-dom';
 class Main extends React.Component {
   render() {
     return(
       <main className="mdl-layout__content">
-        <div className="page-content"><Profile /></div>
+        <div className="page-content">
+          <Route path="/profile" render={ () => <Profile/> } />
+          <Route path="/communities" render={ () => <Profile/> } />
+          <Route path="/awards" render={ () => <Profile/> } />
+        </div>
       </main>
     )
   }
