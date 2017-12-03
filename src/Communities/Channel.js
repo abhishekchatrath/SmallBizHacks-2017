809608232ae706984707bfafa51b87b2af7706e8
Abhishek Chatrath <abhishekchatrath@RBCAppliedInnovations-MacBook-Pro.local>
2017-12-03 07:01:43 -0500

    Add functionality and css to Community channels page

---
+++ src/Communities/Channel.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Channel extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className="Channel">
+
+            </div>
+        );
+    };
+}
+
+export default Channel;
