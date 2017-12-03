6cced4128a0c35d84b6051a2dd5f3a76b1fb4323
Gwen Li <guanyan1995@gmail.com>
2017-12-02 19:40:16 -0500

    modify mianbody.js

--- src/MainContainer/mainbody.js
+++ src/MainContainer/mainbody.js
@@ -1,15 +1,18 @@
 import React  from 'react';
 import './mainbody.css'
 import Profile from '../Profile/profile.js'
-import {Route} from 'react-router-dom';
+import {Route,Redirect} from 'react-router-dom';
 class Main extends React.Component {
   render() {
     return(
       <main className="mdl-layout__content">
         <div className="page-content">
+          <Route exact path="/" render={ () => <Redirect to="/home" />} />
+          <Route path="/home" render={ () => <Profile/> } />
           <Route path="/profile" render={ () => <Profile/> } />
           <Route path="/communities" render={ () => <Profile/> } />
           <Route path="/awards" render={ () => <Profile/> } />
+
         </div>
       </main>
     )
